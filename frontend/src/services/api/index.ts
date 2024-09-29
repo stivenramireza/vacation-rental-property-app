46faf9b25ac7bc8e53a845fd13f633d87eb99fe8
Stiven Ramírez Arango <stivenramireza@gmail.com>
2024-09-29 17:12:07 -0500

    [ADD] REACT_APP_API_URL environment variable

--- frontend/src/services/api/index.ts
+++ frontend/src/services/api/index.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-const BASE_URL = process.env.API_URL;
+const BASE_URL = process.env.REACT_APP_API_URL;
 const CONTENT_TYPE = 'application/json';
 
 const HEADER = {
